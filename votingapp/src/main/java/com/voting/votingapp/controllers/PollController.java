5d139d9822bb8ef76a392daa1a69bb2ae31295df
Karan Rana <103601023+Ranakaran1@users.noreply.github.com>
2025-07-12 00:03:58 +0530

    set up vote increase mrethod

--- votingapp/src/main/java/com/voting/votingapp/controllers/PollController.java
+++ votingapp/src/main/java/com/voting/votingapp/controllers/PollController.java
@@ -1,13 +1,20 @@
 package com.voting.votingapp.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.voting.votingapp.model.Poll;
+import com.voting.votingapp.request.Vote;
 import com.voting.votingapp.services.PollService;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/api/polls")
@@ -19,7 +26,21 @@ public class PollController {
     @PostMapping
     public Poll createPoll(@RequestBody Poll poll) {
         return pollService.createPoll(poll);
+    }
 
+    @GetMapping
+    public List<Poll> getAllPolls(){
+        return pollService.getAllPolls();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Poll> getPoll(@PathParam("id")Long id){
+        return pollService.getPollById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/vote") 
+    public void vote(@RequestBody Vote vote){
+        pollService.vote(vote.getPollId(), vote.getOptionIndex() );
     }
 
 }
--- votingapp/src/main/java/com/voting/votingapp/services/PollService.java
+++ votingapp/src/main/java/com/voting/votingapp/services/PollService.java
@@ -1,8 +1,15 @@
 package com.voting.votingapp.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.voting.votingapp.model.OptionVote;
 import com.voting.votingapp.model.Poll;
 import com.voting.votingapp.repositories.PollRepository;
 
@@ -14,6 +21,37 @@ public class PollService {
 
     public Poll createPoll(Poll poll) {
        return pollRepository.save(poll);
+    }
+
+    public List<Poll> getAllPolls() {
+        return pollRepository.findAll();
+    }
+
+    public Optional<Poll> getPollById(Long id) {
+        
+        return pollRepository.findById(id);
+    }
+
+    public void vote(Long pollId, int optionIndex) {
+        // get the poll from DB
+        Poll poll = pollRepository.findById(pollId).orElseThrow(()->
+            new RuntimeException("Poll not Found") );
+
+        // get all options;
+        List<OptionVote> options = poll.getOptions();
+
+        // check index is valid or not
+        if(pollId<0 || pollId >= options.size()){
+            throw new IllegalArgumentException("Invalid Option Index");
+        }
+
+        OptionVote selectedOption = options.get(optionIndex);
+        selectedOption.setVoteCount(selectedOption.getVoteCount() + 1);
+
+        pollRepository.save(poll);
+
+
+
     }
 
 
